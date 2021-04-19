1d057747c1c0b8a1265ae9f1e30546fd264e9168
Daiichiro Kuroki <32691644+kurokida@users.noreply.github.com>
2021-04-19 11:05:30 +0900

    Minor corrections on 'ii'

--- jsQUEST.js
+++ jsQUEST.js
@@ -584,15 +584,15 @@ function QuestRecompute(q, plotIt, chart_width, chart_height){
         const tmp = Math.round((inten - q.tGuess) / q.grain)
         let ii = numeric.sub(numeric.add(q.pdf.length, q.i), tmp);
         const tmp2 = ii[0]
-        if (tmp2 < 1){
-            ii = numeric.sub(numeric.add(ii, 1), tmp2)
+        if (tmp2 < 0){ // 'ii' must be greater than or equal to zero because 'ii' is the index of an array in JavaScript.
+            ii = numeric.sub(ii, tmp2) 
         }
         const tmp3 = ii[ii.length-1]
-        const tmp4 = numeric.dim(q.s2)[1]
-        if (tmp3 > tmp4){
+        const tmp4 = numeric.dim(q.s2)[1]-1
+        if (tmp3 > tmp4){ // Also, 'ii' must not be greater than the size of an array minus one.
             ii = numeric.sub(numeric.add(ii, tmp4), tmp3)
         }
-        // s2はarray of arrayのはずでよく分からない
+
         // q.pdf = numeric.mul(q.pdf, q.s2[q.response(k)+1, ii]) // 4 ms
         q.pdf = numeric.mul(q.pdf, get_array_using_index(q.s2[q.response[k]], ii))
 
@@ -1017,7 +1017,9 @@ function QuestUpdate(q, intensity, response){
         let ii = numeric.add(tmp, q.i)
 
         // if ii(1)<1 || ii(end)>size(q.s2,2)
-        if (ii[0] < 1 || ii[ii.length-1] > numeric.dim(q.s2)[1]){
+        // 'ii' must be greater than or equal to zero because 'ii' is the index of an array in JavaScript.
+        // Also, 'ii' must not be greater than the size of an array minus one. 
+        if (ii[0] < 0 || ii[ii.length-1] > numeric.dim(q.s2)[1]-1){
             if (q.warnPdf){
                 // low=(1-size(q.pdf,2)-q.i(1))*q.grain+q.tGuess;
                 // high=(size(q.s2,2)-size(q.pdf,2)-q.i(end))*q.grain+q.tGuess;
@@ -1036,11 +1038,10 @@ function QuestUpdate(q, intensity, response){
             // else
             // 	ii=ii+size(q.s2,2)-ii(end);
             // end
-            if (ii[0] < 1){
-                const tmp = numeric.add(ii, 1)
-                ii = numeric.sub(tmp, ii[0]);
+            if (ii[0] < 0){
+                ii = numeric.sub(ii, ii[0]);
             } else {
-                const tmp = numeric.add(ii, numeric.dim(q.s2)[1])
+                const tmp = numeric.add(ii, numeric.dim(q.s2)[1]-1)
                 ii = numeric.sub(tmp, ii[ii.length-1])
             }
         }
