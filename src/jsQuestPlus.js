e60d73558686f843bcdc6482cead035939e8acc1
Daiichiro Kuroki <32691644+kurokida@users.noreply.github.com>
2021-09-28 13:26:15 +0900

    Download QuestPlus for Matlab

---
+++ src/jsQuestPlus.js
@@ -0,0 +1,12 @@
+class QuestPlus {
+    constructor(F, stimDomain, paramDomain, respDomain, stopRule, stopCriterion, minNTrials, maxNTrials){
+
+        // 多次元配列の判定？
+
+        // Fが関数でなかった場合のエラー
+
+        this.F = F
+        this.stimDomain
+
+    }
+}
